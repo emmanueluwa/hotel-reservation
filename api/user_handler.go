db3c5001793ccc664826f229fcd8e83fc9158115
emmanueluwa <emmanuel@fulodev.com>
2024-07-13 16:29:19 +0100

    adding types for users

--- api/user_handler.go
+++ api/user_handler.go
@@ -1,9 +1,16 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/emmanueluwa/hotel-reservation/types"
+	"github.com/gofiber/fiber/v2"
+)
 
 func HandleGetUsers(c *fiber.Ctx) error {
-	return c.JSON("Jack")
+	u := types.User{
+		FirstName: "Jack",
+		LastName: "101",
+	}
+	return c.JSON(u)
 }
 
 func HandleGetUser(c *fiber.Ctx) error {
