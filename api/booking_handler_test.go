4d88de74ca7adfbb4e94250075b59a99550cc239
emmanueluwa <oemmanuel.uwadiae@gmail.com>
2024-07-27 20:25:01 +0100

    adding test for admin access to bookings

--- api/booking_handler_test.go
+++ api/booking_handler_test.go
@@ -3,9 +3,14 @@ package api
 import (
     "testing"
     "time"
-    "fmt"
+    "net/http/httptest"
+    "net/http"
+    "encoding/json"
 
+    "github.com/gofiber/fiber/v2" 
+    "github.com/emmanueluwa/hotel-reservation/types"
     "github.com/emmanueluwa/hotel-reservation/fixtures"
+    "github.com/emmanueluwa/hotel-reservation/middleware"
 )
 
 
@@ -13,13 +18,46 @@ func TestAdminGetBookings(t *testing.T) {
     db := setup(t)
     defer db.teardown(t)
 
-    user := fixtures.AddUser(db.Store, "jack", "stack", false)
-    hotel := fixtures.AddHotel(db.Store, "novotel", "london", 5, nil)
-    room := fixtures.AddRoom(db.Store, "couple", true, 9999.9, hotel.ID)
+    var (
+        adminUser = fixtures.AddUser(db.Store, "admin", "admin", true)
+        user = fixtures.AddUser(db.Store, "jack", "stack", false)
+        hotel = fixtures.AddHotel(db.Store, "novotel", "london", 5, nil)
+        room = fixtures.AddRoom(db.Store, "couple", true, 9999.9, hotel.ID)
+        from = time.Now()
+        till = from.AddDate(0,0,5)
+        booking = fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+        app = fiber.New()
+        admin = app.Group("/", middleware.JWTAuthentication(db.User), middleware.AdminAuth)
+        bookingHandler = NewBookingHandler(db.Store)
+    )
+    
+    admin.Get("/", bookingHandler.HandleGetBookings)
+    req := httptest.NewRequest("GET", "/", nil)
+    req.Header.Add("X-Api-Token", CreateTokenFromUser(adminUser))
+    resp, err := app.Test(req)
+    if err != nil {
+        t.Fatal(err)
+    }
 
-    from := time.Now()
-    till := from.AddDate(0,0,5)
-    booking := fixtures.AddBooking(db.Store, user.ID, room.ID, from, till)
+    if resp.StatusCode != http.StatusOK {
+        t.Fatalf("non 200 response %d", resp.StatusCode)
+    }
 
-    fmt.Println(booking)
+    var bookings []*types.Booking
+    if err := json.NewDecoder(resp.Body).Decode(&bookings); err != nil {
+        t.Fatal(err)
+    }
+    
+    if len(bookings) != 1 {
+        t.Fatalf("expected 1 booking got %d", len(bookings))
+    }
+    
+    have := bookings[0]
+    if have.ID != booking.ID {
+        t.Fatalf("expected %s got %s", booking.ID, have.ID)
+    }
+    
+    if have.UserID != booking.UserID {
+        t.Fatalf("expected %s got %s", booking.UserID, have.UserID)
+    }
 }
